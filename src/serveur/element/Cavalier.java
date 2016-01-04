a56bbf4e1dd07f2d008ba8a54dbf323c414135c1
romain <romain@ordi-romain>
2016-01-04 15:30:56 +0100

    Ajout des classes Assassin, Brute, Sniper, Cavalier et Grenadier

---
+++ src/serveur/element/Cavalier.java
@@ -0,0 +1,21 @@
+package serveur.element;
+
+import java.util.HashMap;
+
+public class Cavalier extends Personnage {
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Cree un personnage avec un nom et un groupe.
+	 * @param nom du personnage
+	 * @param groupe d'etudiants du personnage
+	 * @param caracts caracteristiques du personnage
+	 */
+	public Cavalier(String nom, String groupe, HashMap<Caracteristique, Integer> caracts) {
+		super(nom, groupe, caracts);
+		this.caracts.put(Caracteristique.VIE, 100);
+		this.caracts.put(Caracteristique.FORCE, 80);
+		this.caracts.put(Caracteristique.INITIATIVE, 20);
+	}
+}
---
+++ src/serveur/element/Grenadier.java
@@ -0,0 +1,22 @@
+package serveur.element;
+
+import java.util.HashMap;
+
+public class Grenadier extends Personnage {
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Cree un personnage avec un nom et un groupe.
+	 * @param nom du personnage
+	 * @param groupe d'etudiants du personnage
+	 * @param caracts caracteristiques du personnage
+	 */
+	public Grenadier(String nom, String groupe, HashMap<Caracteristique, Integer> caracts) {
+		super(nom, groupe, caracts);
+		this.caracts.put(Caracteristique.VIE, 100);
+		this.caracts.put(Caracteristique.FORCE, 80);
+		this.caracts.put(Caracteristique.INITIATIVE, 20);
+	}
+}
+
